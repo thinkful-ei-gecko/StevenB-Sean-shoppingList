e6291312db298a1a4d0aa3bb25886ab45ca6b744
Steven Bull <stevenwbulljr@gmail.com>
2019-08-15 17:47:29 -0500

    finished createItem function

---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+
+function createItem() {
+  $('#js-shopping-list-form').on('submit', function(event) {
+    event.preventDefault();
+    const newShoppingEntry = $('#shopping-list-entry').val();
+    $('.shopping-list').append(`
+        <li>
+        <span class="shopping-item">${newShoppingEntry}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`);
+  });
+}
+
+
+function checkItem() {
+
+}
+
+
+function removeItem() {
+
+}
+
+
+$(createItem);
+$(checkItem);
+$(removeItem);
